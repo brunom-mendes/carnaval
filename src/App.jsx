b7b4ab04f50d037fd2c87e14dbaf3cd87a76e8a8
Bruno Miranda Mendes <bruno.mendes@cnj.jus.br>
2026-02-12 18:59:57 -0300

    Revert "feat: pagina standalone Carnaval 2026 com layout single-screen"
    This reverts commit 0d7f65fd28536679dc12d00aea72e44ac3a5b25f.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
-import CarnavalPage from './components/CarnavalPage'
+import { useEffect } from 'react'
+import Hero from './components/Hero'
+import HeaderSticky from './components/HeaderSticky'
+import SectionFade from './components/SectionFade'
+import CtaMontar from './components/CtaMontar'
+import VideoSection from './components/VideoSection'
+import Servicos from './components/Servicos'
+import Espaco from './components/Espaco'
+import Porque from './components/Porque'
+import VideosGaleria from './components/VideosGaleria'
+import Footer from './components/Footer'
+
+function sendHeightToParent() {
+  if (typeof window === 'undefined' || !window.parent || window.parent === window) return
+  const height = document.documentElement.scrollHeight
+  window.parent.postMessage({ type: 'bugu-height', height }, '*')
+}
 
 export default function App() {
-  return <CarnavalPage />
+  useEffect(() => {
+    sendHeightToParent()
+    const ro = new ResizeObserver(sendHeightToParent)
+    ro.observe(document.documentElement)
+    const t = setInterval(sendHeightToParent, 500)
+    return () => {
+      ro.disconnect()
+      clearInterval(t)
+    }
+  }, [])
+
+  return (
+    <>
+      <HeaderSticky />
+      <Hero />
+      <SectionFade>
+        <VideoSection />
+      </SectionFade>
+      <SectionFade>
+        <Servicos />
+      </SectionFade>
+      <SectionFade>
+        <Espaco />
+      </SectionFade>
+      <SectionFade>
+        <CtaMontar />
+      </SectionFade>
+      <SectionFade>
+        <Porque />
+      </SectionFade>
+      <SectionFade>
+        <VideosGaleria />
+      </SectionFade>
+      <Footer />
+    </>
+  )
 }
